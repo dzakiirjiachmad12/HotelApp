f6a9a04f66bf3283375705ee85db575c78af7e66
Dzaki irji achmad <92618861+dzakiirjiachmad12@users.noreply.github.com>
2023-06-13 10:20:16 +0700

    x

--- app/src/main/java/com/dzakiirjiachmad/hotelapp/MainActivity.java
+++ app/src/main/java/com/dzakiirjiachmad/hotelapp/MainActivity.java
@@ -3,12 +3,19 @@ package com.dzakiirjiachmad.hotelapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView _imageView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _imageView1 = (ImageView) findViewById(R.id.imageView1);
+
+        String imageUrl = "https://pantainesia.com/wp-content/uploads/2021/01/Pantai-Samudra-Indah-Bengkayang.jpg";
+        Picasso.with(this).load(imageUrl).into(_imageView1);
     }
 }
